196f391123a8d72f54933e7c99a8dad61bd5a204
julie <1107135021@qq.com>
2022-04-21 17:16:20 +0800

    新建项目

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html {
+  overflow: hidden;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* 设置字体平滑：抗锯齿 */
+  -webkit-font-smoothing: antialiased;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login-cover">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    // msg: '登陆'
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.login-cover {
+  width: 100%;
+  height: 100vh;
+  background: url(../assets/cover.jpg);
+  position: relative;
+}
+.login-center {
+  margin-top: 10px;
+  background: blue;
+}
+</style>
